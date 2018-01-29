8b941bb632b521acc4a61f9b4da8dc9a02eccdba
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-29 08:11:58 -0500

    Updated from Brython Server: 1/29/2018 8:11:57 AM

--- ifDemo.py
+++ ifDemo.py
@@ -1,1 +1,11 @@
+#Sam Krimmel
+#1/29/18
+#ifDemo.py - how to use if, elif, and else.
+
+num = float(input('EnTer A NumBeR: '))
+
+if num > 0:
+    print(num, 'Is PoSiTivE')
+elif num < 0:
+    print(num, 'iS NeGaTivE')
 
