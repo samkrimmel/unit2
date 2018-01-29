cc83c8321126120ab822d9c587fec5e80b170e9c
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-29 08:25:50 -0500

    Updated from Brython Server: 1/29/2018 8:25:49 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,17 @@
+#Sam Krimmel
+#1/29/18
+#gradeCalculator.py - Asks user for grade and outputs letter grade
+
+grade = int(input('What is your grade? '))
+
+if grade >= 90:
+    print('You earned an A')
+elif grade >= 80:
+    print('You earned a B')
+elif grade >= 70:
+    print('You got a C')
+elif grade >= 60:
+    print('You got a D')
+else:
+    print('Bummer, you got an F')
 
