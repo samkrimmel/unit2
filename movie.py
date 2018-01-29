25bd2ea90064a396509f941874a8a1af5c13c121
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-29 08:33:23 -0500

    Updated from Brython Server: 1/29/2018 8:33:22 AM

--- movie.py
+++ movie.py
@@ -1,1 +1,17 @@
+#Sam Krimmel
+#1/29/18
+#movie.py - asks user for age and prints most scandalous movie they can watch
+
+age = int(input('Enter your age: '))
+
+if age > 17:
+    print('You can watch NC-17 movies.')
+elif age >= 17:
+    print('You can watch R rated movies.')
+elif age >= 13:
+    print('You can watch PG-13 movies.')
+elif age >= 10:
+    print('You can watch PG movies')
+else:
+    print('You can watch G rated movies')
 
