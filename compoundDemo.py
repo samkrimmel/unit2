ef3f231292a925aedeb3e29599bf4cc75ef61147
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-30 08:14:20 -0500

    Updated from Brython Server: 1/30/2018 8:14:18 AM

--- compoundDemo.py
+++ compoundDemo.py
@@ -1,3 +1,13 @@
 #Sam Krimmel
 #1/30/18
-#compoundDemo.py - 
+#compoundDemo.py - how to use and/or
+
+num = int(input('Enter a number: '))
+
+if num > 0 and num%7 == 0:
+    print(num, 'is positive and divisible by seven!')
+elif num > 0 and num%7 != 0:
+    print(num, 'is positive but not divisible by seven.
+else:
+    print('ERROR. FBI HAS HACKED YOUR COMPUTER.')
+
