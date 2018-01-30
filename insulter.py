58f3d2d6a5da2590ff78db682a4b7b70d75dcd25
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-30 08:48:28 -0500

    Updated from Brython Server: 1/30/2018 8:48:26 AM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,21 @@
+#Sam Krimmel
+#1/30/18
+#insulter.py - get roasted
+
+from random import randint
+
+name = input('Hello! What is your name?')
+
+num = randint(1,5)
+
+if num == 1:
+    print("Hi", name, "if I had a nickel for every time you said something remotely intellectual, i'd be dirt poor.")
+elif num == 2:
+    print("Is your brain a cucumber? Because, well, you're pretty dumb.")
+elif num == 3:
+    print("I'm surprised you haven't died from a heart attack. I mean, you must've seen your reflection somewhere right?")
+elif num == 4:
+    print("Why do you even bother trying anymore... *sigh*")
+else:
+    print("Your so dumb, you could mistake your for you're.")
 
