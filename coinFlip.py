8adedccd6fc340171d42e30786b91d405c798df2
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-01-31 08:22:08 -0500

    Updated from Brython Server: 1/31/2018 8:22:06 AM

--- coinFlip.py
+++ coinFlip.py
@@ -1,1 +1,13 @@
+#Sam Krimmel
+#1/31/18
+#coinFlip.py - prints out heads or tails
+
+from random import randint
+
+num = randint(1,2)
+
+if num == 1:
+    print('HEAAADDDSSSS')
+else:
+    print('TAAAAAIIIIILLLLSS')
 
