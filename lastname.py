3990cc331120a5f18cee28c1ce2a3d3d6fecc151
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-02 08:47:53 -0500

    Updated from Brython Server: 2/2/2018 8:47:53 AM

--- lastname.py
+++ lastname.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#2/2/18
+#lastname.py - asks user for last name and prints whether it is in the first or second half of the alphabet
 
+name = input('Enter your name: ')
+
+if name >= N:
+    print('Your name is in the second half of the alphabet...')
+elif name <= M:
+    print('Your name is in the first half of the alphabet, YOU ARE SUPERIOR.')
