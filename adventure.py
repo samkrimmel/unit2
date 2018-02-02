bdb86ad6948a206be08d3b76bb7243651d3a1274
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-02 08:19:15 -0500

    Updated from Brython Server: 2/2/2018 8:19:15 AM

--- adventure.py
+++ adventure.py
@@ -5,7 +5,7 @@
 print('You are taking a leisurely ride on train through the mountains of southern France. All of a sudden, a giant Squid Man jumps in front of the tracks, forcing the train to stop.')
 
 print('Do you get out of the train??')
-ans1 = input()
+ans = input()
 
 if 'yes' in ans:
     print("Thank Cthulu you did, Squid Man just crushed the train with a single blow, killing everyone but you and a man who you can't help but think looks exactly like Arnold Schwarzenegger.")
@@ -18,14 +18,14 @@ if 'yes' in ans:
     else:
         print("Uh oh, too late, Squid Man kills you. RIP")
 else:
-    print("You huddle in the corner, Squid Man smashes the train with one blow, but luckily, he just managed to not crush you.)
+    print("You huddle in the corner, Squid Man smashes the train with one blow, but luckily, he just managed to not crush you.")
     
     print("There is shrapnel stuck in your upper arm, do you pull it out?")
     ans3 = input()
     if 'yes' in ans3:
         print("Your arm starts gushing blood like Niagra Falls, and you bleed out within seconds.")
     else:
-        print("It hurts, but you decide to leave it in. You start to climb out of the wreck and there is a very cute looking rabbit sitting in front of you."
+        print("It hurts, but you decide to leave it in. You start to climb out of the wreck and there is a very cute looking rabbit sitting in front of you.")
         
         print("It looks so fluffy and soft, do you pet it?")
         ans4 = input()
