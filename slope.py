a850f667c04afa6dcab0bceb24dd0ef42f6ade6b
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-05 08:06:39 -0500

    Updated from Brython Server: 2/5/2018 8:06:35 AM

--- slope.py
+++ slope.py
@@ -1,1 +1,12 @@
+#Sam Krimmel
+#1/16/18
+#slope.py - calculates the slope between two points
 
+x1 = float(input('x1 = '))
+y1 = float(input('y1 = '))
+x2 = float(input('x2 = '))
+y2 = float(input('y2 = '))
+if x1 == x2:
+    print('The slope is undefined, the equation of the line is X=',x1)
+else:
+    print('The slope is',(y2-y1)/(x2-x1))
