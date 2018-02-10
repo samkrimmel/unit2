2a07904ad7b5d865a863ede3b739e6ed90d63c09
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-10 13:29:35 -0500

    Updated from Brython Server: 2/10/2018 1:29:28 PM

--- ageCalculator.py
+++ ageCalculator.py
@@ -11,16 +11,16 @@ day = int(input('Enter the day you were born: '))
 if date.today().month == month and date.today().day == day:
     print('HAPPY BIRTHDAY!!!')
 elif date.today().year - year < 0:
-    print('You will be born', -1*(date.today().year - year),'years in the future.')
+    print('You will be born', -1*(date.today().year - year),'year(s) in the future.')
 elif date.today().year - year == 0:
     if date.today().month - month < 0:
-        print('You will be born', -1*(date.today().month - month),'months in the future.')
+        print('You will be born', -1*(date.today().month - month),'month(s) in the future.')
     elif date.today().month == month:
         if day > date.today().day:
-            print('You will be born', -1*(date.today().day - day),'days in the future.')
+            print('You will be born', -1*(date.today().day - day),'day(s) in the future.')
         else:
-            print('You are', date.today().day - day,'days old.')
+            print('You are', date.today().day - day,'day(s) old.')
     else:
-        print('You are', date.today().month - month,'months old.')
+        print('You are', date.today().month - month,'month(s) old.')
 else:
-    print('You are', date.today().year - year, 'years old.')
+    print('You are', date.today().year - year, 'year(s) old.')
