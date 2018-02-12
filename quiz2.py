6a984fc977263d337ba555afe3ea821e3592e9ff
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-12 08:19:28 -0500

    Updated from Brython Server: 2/12/2018 8:19:19 AM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,21 @@
+#Sam Krimmel
+#2/12/18
+#quiz2.py - Conditional statements
 
+word1 = input('Enter a word: ')
+word2 = input('Enter another word: ')
+
+if 'p' in word1 and 'p' in word2:
+    print('Both words have a p')
+elif 'p' in word1:
+    print('Only the first word has a p')
+elif 'p' in word2:
+    print('Only the second word has a p')
+
+num1 = int(input('Enter two numbers that add up to 12, enter first number: '))
+num2 = int(input('Enter second number: '))
+
+if num1 + num2 == 12:
+    print('CORRECT!')
+else:
+    print('INCORRECT!')
