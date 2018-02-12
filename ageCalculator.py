51c5052055922d06c0c18d4d1a39b772145adebb
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-12 08:01:11 -0500

    Updated from Brython Server: 2/12/2018 8:01:03 AM

--- ageCalculator.py
+++ ageCalculator.py
@@ -8,19 +8,19 @@ year = int(input('Enter the year in which you were born: '))
 month = int(input('Enter the month in which you were born: '))
 day = int(input('Enter the day you were born: '))
 
-if date.today().month == month and date.today().day == day:
-    print('HAPPY BIRTHDAY!!!')
-elif date.today().year - year < 0:
+if date.today().month == month and date.today().day == day: #Wishes user happy brithday if today is their birthday (regardless of year)
+    print('HAPPY BIRTHDAY!!!') 
+elif date.today().year - year < 0: #Accounts for future births
     print('You will be born', -1*(date.today().year - year),'year(s) in the future.')
-elif date.today().year - year == 0:
-    if date.today().month - month < 0:
+elif date.today().year - year == 0: 
+    if date.today().month - month < 0: #Accounts for future births in current year
         print('You will be born', -1*(date.today().month - month),'month(s) in the future.')
     elif date.today().month == month:
-        if day > date.today().day:
+        if day > date.today().day: #Accounts for future births in the current month
             print('You will be born', -1*(date.today().day - day),'day(s) in the future.')
-        else:
+        else: #Accounts for births in current month
             print('You are', date.today().day - day,'day(s) old.')
-    else:
+    else: #Accounts for births in current year
         print('You are', date.today().month - month,'month(s) old.')
-else:
+else: #Accounts for births outside of current year. 
     print('You are', date.today().year - year, 'year(s) old.')
